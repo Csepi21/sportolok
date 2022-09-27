60f7dd6980d14d42e4d617bf0c31ef9a90c1f0f2
Csepi21 <113436617+Csepi21@users.noreply.github.com>
2022-09-27 12:50:14 +0200

    Add files via upload

---
+++ sebesség.py
@@ -0,0 +1,9 @@
+#A program km/h-t vált át m/s-ba
+#Elemzés: 1 km/h=1000m/3600s=1000/3600 m/s
+#Be: sebesség (km/h)-ban: speed_kmperh
+#Ki: sebesség (m/s)-ban: speed_mpers
+#speed_mpers=speed_kmperh*1000/3600
+speed_kmperh=float(input("Kérem a sebesség számértékét
+speed_mpers=speed_kmperh*1000/3600
+print(speed_mpers."m/s")
+print("Viszlát!")
